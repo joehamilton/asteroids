55de0f8da625776df22bdcd98dc980a0c43af493
joehamilton <joehamilton.info@gmail.com>
2024-11-10 13:51:53 +1100

    add asteroids

--- player.py
+++ player.py
@@ -1,13 +1,16 @@
 import pygame
-from constants import PLAYER_RADIUS
+from constants import *
 from circleshape import CircleShape
+
 
 class Player(CircleShape):
     def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        super().__init__(self.x, self.y, PLAYER_RADIUS)
+        super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
+
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -15,5 +18,22 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
-    def draw(self, screen):
-        pygame.draw.polygon(screen,(255,255,255),self.triangle(),2)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
